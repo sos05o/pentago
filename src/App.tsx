33964b08938ea9e1b2cddd53b90f1dba8ec9b044
sos05o <67443633+sos05o@users.noreply.github.com>
2024-12-18 10:58:13 +0000

    biomeのフォーマットに対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Button, CssBaseline } from "@mui/material";
+import { Button, CssBaseline } from "@mui/material"
 
 function App() {
-	return (
-		<>
-			<CssBaseline />
-			<Button variant="contained">Hello, World!</Button>
-		</>
-	);
+  return (
+    <>
+      <CssBaseline />
+      <Button variant="contained">Hello, World!</Button>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+const root = document.getElementById("root")
+
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+} else {
+  console.error("Root element not found")
+}
