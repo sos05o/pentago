c96d40cb760f2af2434d70ffd2af342d4d8c404e
sos05o <67443633+sos05o@users.noreply.github.com>
2024-12-18 11:01:26 +0000

    path aliasを設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: [{find: "@", replacement: "/src"}]
+	},
 	server: {
 		port: 3000,
 	},
